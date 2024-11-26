8082f00f78802d52e79e7b649149731a0376ff67
Hemant Kumar Yadav <hemantyadav8006@gmail.com>
2024-11-26 11:50:29 +0530

    add getCurrentUser controller

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -231,10 +231,19 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "New Password Save Successfully !"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, req.user, "Current User Fetched Successfully !")
+    );
+});
+
 export {
   registerUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
 };
