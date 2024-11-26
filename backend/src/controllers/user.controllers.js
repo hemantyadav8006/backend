646e61ae7ca073dcef1751876215dd3388f04be4
Hemant Kumar Yadav <hemantyadav8006@gmail.com>
2024-11-26 11:58:15 +0530

    add updateAccountDetails controller

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -239,6 +239,23 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     );
 });
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!(fullName || email)) throw new ApiError(400, "All fields are required!");
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: { fullName: fullName, email: email },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully !"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -246,4 +263,5 @@ export {
   refreshAccessToken,
   changeCurrentPassword,
   getCurrentUser,
+  updateAccountDetails,
 };
